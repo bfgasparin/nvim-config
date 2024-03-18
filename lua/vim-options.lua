dbdc076fd372ed5281925868a3b3d8650699af08
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-18 16:09:02 -0300

    useful highlight

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -28,3 +28,15 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+

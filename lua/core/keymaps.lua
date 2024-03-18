0f597268cb45c4579f54c303690d31c0fb7b6e11
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-17 23:31:24 -0300

    installed first plugins

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,21 @@
+-- set up the leader key
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+-- enable 24-bit colour
+vim.opt.termguicolors = true
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use space for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,3 @@
+require("core.plugin_config.tokyonight")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.lualine")

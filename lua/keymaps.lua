7770bb3725025a45f9ad989aade008ac40972135
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-18 14:41:06 -0300

    added few important keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,9 @@
 vim.keymap.set('n', '<leader>.', '<c-^>', { desc = 'Switch between current and last buffer'})
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search highlight'})
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })

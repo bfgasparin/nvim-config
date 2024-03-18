a6bc1ee96e8d293be04a497ea6883dd5e4341218
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-18 15:38:30 -0300

    added fugitive

---
+++ lua/plugins/git-stuff.lua
@@ -0,0 +1,8 @@
+return   {
+  -- Integration with git commands
+  "tpope/vim-fugitive",
+  {-- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    config = true,
+  }
+}

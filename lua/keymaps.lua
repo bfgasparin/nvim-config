43e6a2f4e0a00f87bc2b13fa538a6481c10c5687
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-18 16:07:33 -0300

    new keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,3 +8,9 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Quickly change windows size
+vim.keymap.set('n', '<left>', '<c-w>3<')
+vim.keymap.set('n', '<right>', '<c-w>3>')
+vim.keymap.set('n', '<up>', '<c-w>3+')
+vim.keymap.set('n', '<down>', '<c-w>3-')

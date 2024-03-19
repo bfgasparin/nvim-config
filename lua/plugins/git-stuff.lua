2dcef2fa39f671e4bfe44df580d72069d24896a4
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-19 18:03:59 -0300

    moved config

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,8 +1,27 @@
 return   {
-  -- Integration with git commands
-  "tpope/vim-fugitive",
-  {-- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    config = true,
+  {
+    -- Integration with git commands
+    "tpope/vim-fugitive",
+    {-- Adds git related signs to the gutter, as well as utilities for managing changes
+      'lewis6991/gitsigns.nvim',
+      config = true,
+    }
+  },
+  { -- git terminal UI
+    "kdheepak/lazygit.nvim",
+    keys = {
+      {'<leader>g', '<cmd>LazyGit<CR>', { desc = 'Open Lazy[G]it UI'}},
+    },
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    }
   }
 }

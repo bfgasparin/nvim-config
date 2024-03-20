d78b46184ec8dd90dd5310be7b9bbab5e549affc
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-20 19:08:52 -0300

    new telescope window

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[F]ind existing [B]uffers' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = 'Find Files' })
+      vim.keymap.set('n', '<c-g>', builtin.git_status, { desc = 'Find in Git Status' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
 
       -- Find file in vendor

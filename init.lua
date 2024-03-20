2be3d8880582f575a5ea40206a9feabed5a100d1
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-20 16:00:47 -0300

    added dependencies comment

--- init.lua
+++ init.lua
@@ -1,6 +1,21 @@
 require("vim-options")
 require("keymaps")
 
+--[[
+=====================================================================
+====================== EXTERNAL REQUIREMENTS ========================
+=====================================================================
+
+External Requirements:
+- Basic utils: `git`, `make`, `unzip`, C Compiler (`gcc`)
+- [ripgrep](https://github.com/BurntSushi/ripgrep#installation)
+- [fd](https://github.com/sharkdp/fd)
+- [node.js](https://github.com/nodejs/node) version 18.12.0 or higher
+- A [Nerd Font](https://www.nerdfonts.com/): optional, provides various icons
+  - if you have it set `vim.g.have_nerd_font` in `init.lua` to true
+
+]]--
+
 -- install package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then

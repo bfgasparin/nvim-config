036192ae483432daa42166081448960da15f8ebe
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-20 00:18:38 -0300

    fixed configs

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,10 +1,9 @@
 return   {
-  {
-    -- Integration with git commands
-    "tpope/vim-fugitive",
-    {-- Adds git related signs to the gutter, as well as utilities for managing changes
-      'lewis6991/gitsigns.nvim',
-    }
+  -- Integration with git commands
+  "tpope/vim-fugitive",
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {},
   },
   { -- git terminal UI
     "kdheepak/lazygit.nvim",

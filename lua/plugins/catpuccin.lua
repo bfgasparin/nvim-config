bd8b8d312c7eaade7c6c562cadd9fc4f7e7b5b0f
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-20 01:28:59 -0300

    indenting

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,14 +1,14 @@
 -- the colorscheme should be available when starting Neovim
 return {
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    opts = {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    },
-    config = function(plugin, opts) 
-      require'catppuccin'.setup(opts)
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000,
+  opts = {
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+  },
+  config = function(plugin, opts) 
+    require'catppuccin'.setup(opts)
 
-      vim.cmd.colorscheme "catppuccin"
-    end
+    vim.cmd.colorscheme "catppuccin"
+  end
 }

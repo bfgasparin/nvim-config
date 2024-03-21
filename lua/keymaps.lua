ebc80194861f34ed7e0ef9529ae2e07b4ad45fae
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-21 05:16:48 -0300

    more keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,14 +4,18 @@
 -- switch between current and last buffer
 vim.keymap.set('n', '<leader>.', '<C-^>', { desc = 'Switch between current and last buffer'})
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search highlight'})
+vim.keymap.set('n', '<leader>h', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight'})
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist, { desc = 'Open diagnostic in [L]ocation list' })
+vim.keymap.set('n', '<leader>q', function()
+    local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
+    local action = qf_winid > 0 and 'cclose' or 'copen'
+    vim.cmd('botright '..action)
+end, { desc = 'Open [Q]uickfix list' })
 
 -- Quickly change windows size
 vim.keymap.set('n', '<left>', '<C-w>3<')
@@ -23,7 +27,6 @@ vim.keymap.set('n', '<down>', '<C-w>3-')
 vim.keymap.set('n', '<leader>d', '<cmd>bd<CR>')
 
 -- quickfix windoe
-vim.keymap.set('n', '<leader>wq', '<cmd>copen<CR>')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands

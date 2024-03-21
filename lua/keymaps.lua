070167e69964cc581270abe0985ab170820e64d9
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-03-21 04:40:16 -0300

    fixed keymapping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,6 +11,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Quickly change windows size
 vim.keymap.set('n', '<left>', '<C-w>3<')

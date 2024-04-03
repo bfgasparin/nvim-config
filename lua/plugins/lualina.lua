1a893e0487a7ab55c43a5ec001dd014b290a554f
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-04-03 04:40:03 -0300

    better lualine

--- lua/plugins/lualina.lua
+++ lua/plugins/lualina.lua
@@ -5,14 +5,30 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'dracula',
+      theme = 'auto',
       disabled_filetypes = { 'NvimTree' },
     },
     sections = {
+      -- lualine_a = {'branch', 'diff', 'diagnostics'},
       lualine_c = {
         {
-          'filename',
-          path = 1,
+          'buffers',
+          show_filename_only = true,   -- Shows shortened relative path when set to false.
+          hide_filename_extension = false,   -- Hide filename extension when set to true.
+          show_modified_status = true, -- Shows indicator when the buffer is modified.
+
+          mode = 4, -- 0: Shows buffer name
+          -- 1: Shows buffer index
+          -- 2: Shows buffer name + buffer index
+          -- 3: Shows buffer number
+          -- 4: Shows buffer name + buffer number
+
+          max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+          -- it can also be a function that returns
+          -- the value of `max_length` dynamically.
+
+          -- Automatically updates active window color to match color of other components (will be overidden if buffers_color is set)
+          use_mode_colors = false,
         }
       },
     },

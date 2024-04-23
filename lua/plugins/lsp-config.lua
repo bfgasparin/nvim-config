448d6bb0bda809eac4b26c604e36ff49e961b845
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2024-04-23 13:58:31 -0300

    configured new lsp servers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,12 +8,16 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      ensure_installed = { "lua_ls", "tsserver", "intelephense", }
+      ensure_installed = { "lua_ls", "intelephense", "tsserver", "volar", "tailwindcss" }
     },
   },
   { -- LSP Configuration & Plugins
     "neovim/nvim-lspconfig",
     config = function()
+
+      -- for default servers configuration, check:
+      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+      
       -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers.
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -22,11 +26,40 @@ return {
       lspconfig.lua_ls.setup {
         capabilities = capabilities
       }
-      lspconfig.tsserver.setup {
-        capabilities = capabilities
-      }
       lspconfig.intelephense.setup {
         capabilities = capabilities
+      }
+      lspconfig.tailwindcss.setup {
+        capabilities = capabilities
+      }
+
+      --
+      -- Run `volar` in hybrid mode. d. In this mode, the volar exclusively manages the CSS/HTML section for `.vue`
+      -- files and tsserver manages the rest.  tsserver still is resmposible for `.ts` and `.js` files.
+      -- For more details check: 
+      --   - https://github.com/vuejs/language-tools/blob/e0f2d43f9aab17eb27e28893aa1ffcade51927b6/README.md?plain=1#L61
+      --   - https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/volar.lua
+      --   - https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/tsserver.lua
+      --
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              -- IMPORTANT: It is crucial to ensure that @vue/typescript-plugin and volar are of identical versions.
+              location = "/Users/brunofermegasparin/Library/pnpm/global/5/node_modules/@vue/typescript-plugin",
+              languages = {"javascript", "typescript", "vue"},
+            },
+          },
+        },
+        filetypes = {
+          "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue",
+        },
+      }
+      -- No need to set `hybridMode` to `true` as it's the default value
+      lspconfig.volar.setup {
+        capabilities = capabilities,
       }
 
       --  This function gets run when an LSP attaches to a particular buffer.
